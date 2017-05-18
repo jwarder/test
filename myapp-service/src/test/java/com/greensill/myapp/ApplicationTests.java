af5faeb8de9ac873d47a7559890a53f33b468829
Jack Warder <jack.warder@greensill.com>
2017-05-18 10:10:23 +0100

    updated

--- myapp-service/src/test/java/com/greensill/myapp/ApplicationTests.java
+++ myapp-service/src/test/java/com/greensill/myapp/ApplicationTests.java
@@ -1,16 +1,22 @@
 package com.greensill.myapp;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
 public class ApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void testSomethingWorks() {
+		assertTrue(true);
 	}
 
+	@Test
+	public void testSomethingDoesntWork() {
+		assertTrue(false);
+	}
+	
 }
